ee3477000c62c59ac346d31b858234fab7a5e8ba
桃花 坂口 <C3122@oic.jp>
2024-10-11 13:45:34 +0900

    a

---
+++ SalesManagement_SysDev/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement_SysDev
+{
+    internal class Class1
+    {
+        public List<int> register(int touroku)
+        {
+            return new List<int>();//new class1()で作られる
+
+        }
+    }
+}
---
+++ SalesManagement_SysDev/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement_SysDev
+{
+    internal class Class2
+    {
+        public void a (int i) 
+        {
+            Class1 class1 = new Class1();
+            class1.register(2);
+        }
+
+    }
+}
