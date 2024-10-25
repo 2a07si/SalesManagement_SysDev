735afa642998a294db9e3219007e38fb7d971c74
桃花 坂口 <C3122@oic.jp>
2024-10-25 16:53:53 +0900

    era-kaijo

--- SalesManagement_SysDev/juchuu~uriage/accepting orders.cs
+++ SalesManagement_SysDev/juchuu~uriage/accepting orders.cs
@@ -133,9 +133,6 @@ namespace SalesManagement_SysDev
         {
             CurrentStatus.ResetStatus(label2);
         }
-
-<<<<<<< HEAD
-=======
         private void b_kakutei_Click_1(object sender, EventArgs e)
         {
             // 現在の状態を確認   
@@ -165,7 +162,6 @@ namespace SalesManagement_SysDev
                     break;
             }
         }
->>>>>>> 8506cc025c0e0473fd343d6ce157ad3d1543c496
 
         private void UpdateOrder()
         {
@@ -307,11 +303,7 @@ namespace SalesManagement_SysDev
             {
                 MessageBox.Show("検索中にエラーが発生しました: " + ex.Message);
             }
-
-<<<<<<< HEAD
-=======
         }
->>>>>>> 8506cc025c0e0473fd343d6ce157ad3d1543c496
         // DataGridViewのセルがクリックされたときのイベントハンドラ
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -320,10 +312,6 @@ namespace SalesManagement_SysDev
             {
                 // クリックした行のデータを取得
                 var row = dataGridView1.Rows[e.RowIndex];
-<<<<<<< HEAD
-
-=======
->>>>>>> 8506cc025c0e0473fd343d6ce157ad3d1543c496
                 // 各テキストボックスにデータを設定
                 TBJyutyuID.Text = row.Cells["受注ID"].Value.ToString();
                 TBShopID.Text = row.Cells["営業所ID"].Value.ToString();
@@ -335,21 +323,5 @@ namespace SalesManagement_SysDev
                 DelFlag.Checked = row.Cells["非表示フラグ"].Value.ToString() == "1"; // 非表示フラグの設定
             }
         }
-
-<<<<<<< HEAD
     }
 }
-=======
-        private void b_ord_Click(object sender, EventArgs e)
-        {
-            formChanger.NavigateToOrderForm();
-        }
-
-        private void colord_Click(object sender, EventArgs e)
-        {
-
-        }
-    }
-}
-
->>>>>>> 8506cc025c0e0473fd343d6ce157ad3d1543c496
