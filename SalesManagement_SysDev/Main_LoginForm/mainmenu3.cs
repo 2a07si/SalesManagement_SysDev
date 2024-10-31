762c563eb4f84c948a92b622da2c9ac0d6509d4a
出雲 北島 <C3095@oic.jp>
2024-10-31 13:24:55 +0900

    a

---
+++ SalesManagement_SysDev/Main_LoginForm/mainmenu3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesManagement_SysDev.Main_LoginForm
+{
+    public partial class mainmenu : Form
+    {
+        public mainmenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
