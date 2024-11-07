9610746c8fb6f988d838c219d13c101249709df9
恭也 乾 <C3027@oic.jp>
2024-11-07 15:57:43 +0900

    いぬい

--- SalesManagement_SysDev/Main_LoginForm/F_login.Designer.cs
+++ SalesManagement_SysDev/Main_LoginForm/F_login.Designer.cs
@@ -177,7 +177,6 @@
             button1.TabIndex = 11;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
-            button1.Visible = false;
             button1.Click += button1_Click;
             // 
             // button2
@@ -188,7 +187,6 @@
             button2.TabIndex = 12;
             button2.Text = "button2";
             button2.UseVisualStyleBackColor = true;
-            button2.Visible = false;
             button2.Click += button2_Click;
             // 
             // button3
@@ -199,7 +197,6 @@
             button3.TabIndex = 13;
             button3.Text = "button3";
             button3.UseVisualStyleBackColor = true;
-            button3.Visible = false;
             button3.Click += button3_Click;
             // 
             // F_login
