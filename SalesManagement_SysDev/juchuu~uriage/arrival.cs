66a91bfeecf64559833241f2e5863bde7cddefba
出雲 北島 <C3095@oic.jp>
2024-11-26 14:40:49 +0900

    test

--- SalesManagement_SysDev/juchuu~uriage/arrival.cs
+++ SalesManagement_SysDev/juchuu~uriage/arrival.cs
@@ -1159,15 +1159,15 @@ namespace SalesManagement_SysDev
                     tbfalse();
                     break;
                 default:
-                    TBNyuukaID.BackColor = SystemColors.Window;
-                    TBShopID.BackColor = SystemColors.Window;
-                    TBShainID.BackColor = SystemColors.Window;
-                    TBKokyakuID.BackColor = SystemColors.Window;
-                    TBJyutyuID.BackColor = SystemColors.Window;
-                    TBNyukaSyosaiID.BackColor = SystemColors.Window;
-                    TBNyuukaIDS.BackColor = SystemColors.Window;
-                    TBSyohinID.BackColor = SystemColors.Window;
-                    TBSuryou.BackColor = SystemColors.Window;
+                    TBNyuukaID.BackColor = Color.FromArgb(252, 252, 252);
+                    TBShopID.BackColor = Color.FromArgb(252, 252, 252);
+                    TBShainID.BackColor = Color.FromArgb(252, 252, 252);
+                    TBKokyakuID.BackColor = Color.FromArgb(252, 252, 252);
+                    TBJyutyuID.BackColor = Color.FromArgb(252, 252, 252);
+                    TBNyukaSyosaiID.BackColor = Color.FromArgb(252, 252, 252);
+                    TBNyuukaIDS.BackColor = Color.FromArgb(252, 252, 252);
+                    TBSyohinID.BackColor = Color.FromArgb(252, 252, 252);
+                    TBSuryou.BackColor = Color.FromArgb(252, 252, 252);
                     break;
             }
         }
