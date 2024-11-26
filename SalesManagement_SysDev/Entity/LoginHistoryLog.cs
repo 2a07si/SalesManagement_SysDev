efe421cd8f3acaa5c33abdd939a75b0bb26b4220
恭也 乾 <C3027@oic.jp>
2024-11-26 13:30:15 +0900

    a

--- SalesManagement_SysDev/Entity/LoginHistoryLog.cs
+++ SalesManagement_SysDev/Entity/LoginHistoryLog.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +13,6 @@ namespace SalesManagement_SysDev.Entity
         // ログインID
         public string LoginID { get; set; }
 
-        public string UserName { get; set; }
 
         // パスワード
         public string Password { get; set; }
