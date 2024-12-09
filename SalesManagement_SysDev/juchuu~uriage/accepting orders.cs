93bbe72282892c368f72227ed2763a2b42794676
桃花 坂口 <C3122@oic.jp>
2024-12-09 09:00:17 +0900

    a

--- SalesManagement_SysDev/juchuu~uriage/accepting orders.cs
+++ SalesManagement_SysDev/juchuu~uriage/accepting orders.cs
@@ -105,7 +105,6 @@ namespace SalesManagement_SysDev
             checkBoxDateFilter.Checked = false;
             checkBox_2.Checked = false;
             colorReset();
-            UpdateTextBoxState(checkBoxSyain.Checked);
         }
 
         private void b_ser_Click(object sender, EventArgs e)
@@ -1718,6 +1717,7 @@ namespace SalesManagement_SysDev
             else
             {
                 TBShainID.Enabled = true; // 有効化
+                TBShainID.Text = "";
             }
 
             // フラグをオフに戻す
