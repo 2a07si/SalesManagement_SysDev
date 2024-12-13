aa9d9c335de8f5b8445975e0ee618260dd870e2a
池田 賢人 <C3015@oic.osaka.jp>
2024-12-13 14:31:11 +0900

    いったん

---
+++ SalesManagement_SysDev/Classまとめ/Kuraberu_kun.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement_SysDev.Classまとめ
+{
+    internal class Kuraberu_kun
+    {
+    }
+}
